a345f306c26793e797e7888c2c22c783c957c9ae
Jiahui Wang <wjh4289042@hotmail.com>
2019-12-03 17:16:47 -0800

    serverjs

--- public/assets/server.js
+++ public/assets/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
+const fs = require ("fs");
 
 // Sets up the Express App
 const app = express();
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //empty notes at the beginning
-let notes = [];
+const notes = [];
 
 // Routes
 // Basic route that sends the user first to the AJAX Page
@@ -19,13 +20,41 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 // Displays all notes ???
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
+app.get("/api/notes", function(req, res) {
+  return res.json(notes);
+});
+
+// ???? Displays a single note, or returns false
+app.get("/api/notes/:note", function(req, res) {
+    var chosen = req.params.character;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < notes.length; i++) {
+      if (chosen === notes[i].routeName) {
+        return res.json(notes[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+
+//create new post
+app.post("/api/notes", function(req, res){
+    let newNote = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newNote
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newNote.name = newNote.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newNote);
+    notes.push(newNote);
+    res.json(newNote);
 });
 
 
